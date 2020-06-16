824441733f3617da056af9e1a062468226538027
Darkwolfxj <hesitation.dotdot@gmil.com>
2020-06-16 18:33:14 -0400

    finished mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,21 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    # Your code here
+def binary_search(arr, target, start=0, end=None):
+    if end is None:
+        end = len(arr) - 1
 
+    if start > end:
+        return -1
+        
+    mid = (start + end) // 2
 
+    if target is arr[mid]:
+        return mid 
+
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid - 1)
+
+    return binary_search(arr, target, mid + 1, end)
+        
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
 # the target regardless of whether the input array is
@@ -10,5 +23,4 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,52 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-
-    # Your code here
-
-
+# TO-DO: complete the helper function below to merge 2 sorted leftys
+def merge(left, right):
+    elements = len(left) + len(right)
+    merged_arr = [0] * elements 
+    # set up a counter
+    index = 0
+    # compare first index of each lefty
+    while len(left) > 0 or len(right) > 0:
+        try:
+    # if a is less, overwrite current index in merged with a[0], else overwrite with b[0]
+            if left[0] < right[0]:
+                merged_arr[index] = left.pop(0)
+    # increment counter and pop index that was added
+                index += 1
+            else:
+                merged_arr[index] = right.pop(0)
+                index += 1
+    # recursively compare until all indeces shared between the two lists have been compared
+        except:
+    # if len(a) is 0 or len(b) is 0, overwrite the rest of merged with the remainder
+            if len(left) == 0:
+                for i in range(len(right)):
+                    merged_arr[index] = right.pop(0)
+                    index += 1
+            if len(right) == 0:
+                for i in range(len(left)):
+                    merged_arr[index] = left.pop(0)
+                    index += 1
+    # return merged
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
-
-    return arr
-
+    # break lefty in half, recursively until lefty lengths are 1
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        lsl = merge_sort(arr[:mid])
+        rsl = merge_sort(arr[mid:])
+        return merge(lsl, rsl)    
+    return arr    
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
 
